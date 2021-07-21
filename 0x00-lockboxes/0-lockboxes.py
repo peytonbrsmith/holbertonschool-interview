070a57fd5e614a7657600a112fbef43bc7e18e68
Peyton Smith <peytonbrsmith@gmail.com>
2021-07-21 16:45:42 -0500

    fixed edge cases

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -6,33 +6,26 @@ def canUnlockAll(boxes):
     """ Determins if ALL boxes are unlockable """
     box_num = 0
     keys_avail = []
-    locked_boxes = []
+    locked_boxes = boxes.copy()
     for box in boxes:
         if box_num == 0:
             for key in box:
-                keys_avail.append(key)
+                if key not in keys_avail:
+                    keys_avail.append(key)
+            locked_boxes.remove(box)
         else:
             if box_num in keys_avail:
                 for key in box:
-                    keys_avail.append(key)
+                    if key not in keys_avail:
+                        keys_avail.append(key)
                 for lbox in locked_boxes:
                     if box_num in keys_avail:
                         for key in box:
-                            keys_avail.append(key)
+                            if key not in keys_avail:
+                                keys_avail.append(key)
                         locked_boxes.remove(lbox)
-            else:
-                locked_boxes.append(box)
         box_num += 1
-    if locked_boxes == []:
+    if locked_boxes == [] and len(keys_avail) == len(boxes) - 1:
         return True
     else:
         return False
-
-# boxes = [[1], [2], [3], [4], []]
-# print(canUnlockAll(boxes))
-
-# boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-# print(canUnlockAll(boxes))
-
-# boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-# print(canUnlockAll(boxes))
