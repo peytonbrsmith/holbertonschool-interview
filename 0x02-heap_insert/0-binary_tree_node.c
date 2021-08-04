3cfcf2836be9edadffa911f80df5e974e5031fde
Peyton Smith <peytonbrsmith@gmail.com>
2021-08-04 21:24:40 +0000

    added new project

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,44 @@
+#include "binary_trees.h"
+
+/**
+  * 
+  *
+  *
+  */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+  binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+  if (new_node == NULL)
+  {
+    return (NULL);
+  }
+
+  new_node->n = value;
+  new_node->left = NULL;
+  new_node->right = NULL;
+
+  if (parent == NULL)
+  {
+    new_node->parent = NULL;
+    parent = new_node;
+    return (new_node);
+  }
+  else if (parent->left == NULL)
+  {
+    new_node->parent = parent;
+    parent->left = new_node;
+    return (new_node);
+  }
+  else if (parent->right == NULL)
+  {
+    new_node->parent = parent;
+    parent->right = new_node;
+    return(new_node);
+  }
+  else
+  {
+    free(new_node);
+    return (NULL);
+  }
+
+}
