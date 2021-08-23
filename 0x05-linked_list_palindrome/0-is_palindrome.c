04fb51f54efb2f5f63fcd4baed5ea3e75b077954
peytonbrsmith <peytonbrsmith@gmail.com>
2021-08-23 16:58:53 -0500

    updated task

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -6,44 +6,37 @@
  * is_palindrome - check if a linked list is a palindrome
  * @head: head of the linked list
  *
- * Return: 1 if the linked list is a palindrome, 0 otherwise
+ * Return: Always 0.
  */
 int is_palindrome(listint_t **head)
 {
-    listint_t *current = *head;
-    int *temp = NULL;
-    int i = 0, j = 0, length = 0;
+	listint_t *current = *head;
+	int *temp = NULL;
+	int i = 0, j = 0, length = 0;
 
-    if (head == NULL)
-    {
-        return 0;
-    }
-    while (current)
-    {
-        current = current->next;
-        length++;
-    }
-
-    temp = (int *)malloc(sizeof(int) * length);
-    if (temp == NULL) {
-        printf("Memory not allocated.\n");
-        return 0;
-    }
-    current = *head;
-    for (i = 0; current != NULL; i++)
-    {
-        *(temp + i) = current->n;
-        current = current->next;
-    }
-
-    for (i = 0, j = length - 1; i < length; ++i, --j) {
-        if (*(temp + i) != *(temp + j)) {
-            free(temp);
-            return 0;
-        }
-    }
-
-    free(temp);
-
-    return (1);
+	if (head == NULL)
+		return (1);
+	temp = (int *)malloc(sizeof(int));
+	if (temp == NULL)
+		return (0);
+	for (i = 0, current = *head; current != NULL; i++, current = current->next)
+	{
+		*(temp + i) = current->n;
+		if (current->next != NULL)
+		{
+			temp = (int *)realloc(temp, sizeof(int) * (i + 2));
+			if (temp == NULL)
+				return (0);
+		}
+	}
+	for (length = i, i = 0, j = length - 1; i < length; ++i, --j)
+	{
+		if (*(temp + i) != *(temp + j) || length % 2 != 0)
+		{
+			free(temp);
+            return (0);
+		}
+	}
+	free(temp);
+	return (1);
 }
