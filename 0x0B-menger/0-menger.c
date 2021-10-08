be08bc85dbf9b2c1551c04a90f4048973286df94
peytonbrsmith <peytonbrsmith@gmail.com>
2021-10-07 19:30:16 -0500

    update

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -10,29 +10,28 @@
  */
 void menger(int level)
 {
-    int x, y, space;
-    int sponge_size = pow(3, level);
-    char prc;
+	int y = 0, x = 0, space = 1;
+	int sponge_size = pow(3, level);
+	char print = '#';
 
 	if (level < 0)
 		return;
 
-	for (space = 1, prc = '#'; y < sponge_size; y++)
+	for (; y < sponge_size; y++)
 	{
 		for (x = 0; x < sponge_size; x++)
 		{
-            prc = '#';
 			space = 1;
-
-            for (; space < sponge_size; space *= 3)
-            {
+			print = '#';
+			for (; space < sponge_size; space *= 3)
+			{
 				if ((y / space) % 3 == 1 && (x / space) % 3 == 1)
 				{
-					prc = ' ';
+					print = ' ';
 					break;
 				}
 			}
-			printf("%c", prc);
+			printf("%c", print);
 		}
 		printf("\n");
 	}
