47acb5b9f39f5e0082044c7cbeeb5a0247dee01f
peytonbrsmith <peytonbrsmith@gmail.com>
2021-10-25 18:13:38 -0500

    added project

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -15,11 +15,9 @@ avl_t *create_tree(int *array, int start, int end, avl_t *root)
     if (start > end)
         return NULL;
 
-    //Compute the middle index and make it as root.
     mid = (start + end)/2;
     root = create_leaf(root, array[mid]);
 
-    //Recursively create left and right subtrees.
     root->left = create_tree(array, start, mid - 1, root);
     root->right = create_tree(array, mid + 1, end, root);
     return root;
