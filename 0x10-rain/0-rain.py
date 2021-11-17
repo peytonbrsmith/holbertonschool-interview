e895bdc6f04d4b60de3c357c356569e9f8af69a5
peytonbrsmith <peytonbrsmith@gmail.com>
2021-11-17 14:58:18 -0600

    added task

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -6,4 +6,15 @@
 
 def rain(walls):
     """rain function"""
-
+    if (len(walls) <= 0):
+        return (0)
+    water = 0
+    for x in range(1, len(walls) - 1):
+        wall_1 = walls[x]
+        wall_2 = walls[x]
+        for y in range(x):
+            wall_1 = max(wall_1, walls[y])
+        for y in range(x + 1, len(walls)):
+            wall_2 = max(wall_2, walls[y])
+        water += min(wall_1, wall_2) - walls[x]
+    return (water)
