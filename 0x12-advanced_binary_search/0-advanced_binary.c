7c7ff293dc3af45b0febb3f0557034aeac34e02e
peytonbrsmith <peytonbrsmith@gmail.com>
2021-12-01 22:13:06 -0600

    added project files

---
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * advanced_binary - binary search
+ * @array: array to search
+ * @size: size of array
+ * @value: value to search for
+ *
+ * Return: index of value, -1 if not found
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+    if (array == NULL || size < 1)
+        return (-1);
+
+    // recursive binary search
+    return (binary_search(array, 0, size - 1, value));
+}
+
+int binary_search(int *array, int left, int right, int value)
+{
+    int mid = (left + right) / 2;
+    int i;
+
+    if (left > right)
+        return (-1);
+    else
+        printf("Searching in array: ");
+
+    for (i = left; i <= right; i++)
+    {
+        if (i < right)
+            printf("%d, ", array[i]);
+        else
+            printf("%d\n", array[i]);
+    }
+
+    if (array[left] == value)
+        return (left);
+
+    if (array[mid] < value)
+        return (binary_search(array, mid + 1, right, value));
+
+    return (binary_search(array, left, mid, value));
+}
---
+++ 0x12-advanced_binary_search/README.md
@@ -0,0 +1,2 @@
+# 0x12-advanced_binary_search
+This directory id for Holberton's 0x12-advanced_binary_search project
