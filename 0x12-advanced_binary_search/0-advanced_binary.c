21127f39ddfd6b1bdf3adc7a36a314b2a9167b7d
peytonbrsmith <peytonbrsmith@gmail.com>
2021-12-01 22:16:44 -0600

    fixed betty

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -12,35 +12,43 @@
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-    if (array == NULL || size < 1)
-        return (-1);
+	if (array == NULL || size < 1)
+		return (-1);
 
-    return (binary_search(array, 0, size - 1, value));
+	return (binary_search(array, 0, size - 1, value));
 }
 
+/**
+ *binary_search - binary search
+ * @array: array to search
+ * @left: left index
+ * @right: right index
+ * @value: value to search for
+ *
+ * Return: index of value, -1 if not found
+ */
 int binary_search(int *array, int left, int right, int value)
 {
-    int mid = (left + right) / 2;
-    int i;
+	int mid = (left + right) / 2;
+	int i;
 
-    if (left > right)
-        return (-1);
-    else
-        printf("Searching in array: ");
+	if (left > right)
+		return (-1);
+	printf("Searching in array: ");
 
-    for (i = left; i <= right; i++)
-    {
-        if (i < right)
-            printf("%d, ", array[i]);
-        else
-            printf("%d\n", array[i]);
-    }
+	for (i = left; i <= right; i++)
+	{
+		if (i < right)
+			printf("%d, ", array[i]);
+		else
+			printf("%d\n", array[i]);
+	}
 
-    if (array[left] == value)
-        return (left);
+	if (array[left] == value)
+		return (left);
 
-    if (array[mid] < value)
-        return (binary_search(array, mid + 1, right, value));
+	if (array[mid] < value)
+		return (binary_search(array, mid + 1, right, value));
 
-    return (binary_search(array, left, mid, value));
+	return (binary_search(array, left, mid, value));
 }
