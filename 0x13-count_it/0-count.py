2e850e0223cff5657d2abab3f0937d77f1337db6
peytonbrsmith <peytonbrsmith@gmail.com>
2021-12-09 19:33:52 -0600

    fixed word counting

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -10,8 +10,6 @@ import sys
 def count_words(subreddit, word_list=[], after=None, count_dict=None):
     """ returns a list containing the titles of
     all hot articles for a given subreddit """
-    for word in word_list:
-        word = word.lower()
     if after is None:
         subreddit_exists = requests.get(
             "https://reddit.com/r/{}".format(subreddit),
@@ -21,14 +19,16 @@ def count_words(subreddit, word_list=[], after=None, count_dict=None):
                 "https://reddit.com/r/{}/hot.json?limit=100".format(subreddit),
                 headers={'User-agent': 'test'})
             children = first_hot.json().get("data").get("children")
+            if (count_dict is None):
+                count_dict = dict.fromkeys(word_list, 0)
             for child in children:
-                title = first_hot.json().get("data").get(
-                    "children")[0].get("data").get("title")
-                if (count_dict is None):
-                    count_dict = dict.fromkeys(word_list, 0)
-                for word in word_list:
-                    count = title.lower().count(word + " ")
-                    count_dict[word] += count
+                for word in child['data']['title'].split():
+                    for key_word in word_list:
+                        if key_word.lower() == word.lower():
+                            if key_word.lower() in count_dict.keys():
+                                count_dict[key_word.lower()] += 1
+                            else:
+                                count_dict[key_word.lower()] = 1
             after = first_hot.json().get("data").get("after")
             count_words(subreddit, word_list, after, count_dict)
         else:
@@ -40,15 +40,17 @@ def count_words(subreddit, word_list=[], after=None, count_dict=None):
             headers={'User-agent': 'test'})
         children = next_hot.json().get("data").get("children")
         for child in children:
-            title = next_hot.json().get("data").get(
-                "children")[0].get("data").get("title")
-            for word in word_list:
-                count = title.lower().count(word + " ")
-                count_dict[word] += count
+            for word in child['data']['title'].split():
+                for key_word in word_list:
+                    if key_word.lower() == word.lower():
+                        if key_word.lower() in count_dict.keys():
+                            count_dict[key_word.lower()] += 1
+                        else:
+                            count_dict[key_word.lower()] = 1
         after = next_hot.json().get("data").get("after")
         if after is not None:
             return count_words(subreddit, word_list, after, count_dict)
-        for key, value in sorted(count_dict.items(), key=lambda x: x[1]):
-            if (value > 0):
-                print("{}: {}".format(key, value))
+        for key in sorted(count_dict, key=lambda x: (-count_dict[x], x)):
+            if count_dict[key] > 0:
+                print('{}: {}'.format(key, count_dict[key]))
         return (count_dict)
