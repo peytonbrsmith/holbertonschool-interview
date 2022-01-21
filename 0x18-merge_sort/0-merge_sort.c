a9d6d26e699aebe71cc6ee5dc4c624cf61c9db6a
peytonbrsmith <peytonbrsmith@gmail.com>
2022-01-20 21:42:37 -0600

    updated file

--- 0x18-merge_sort/0-merge_sort.c
+++ 0x18-merge_sort/0-merge_sort.c
@@ -59,7 +59,13 @@ void Merge(int array[], int l_start, int mid, int r_end, int new_array[])
 		}
 	}
 	printf("[Done]: ");
-	print_array(array, r_end);
+	while (array && i < mid)
+	{
+		if (i > l_start)
+			printf(", ");
+		printf("%d", array[i]);
+		++i;
+	}
 }
 
 /**
