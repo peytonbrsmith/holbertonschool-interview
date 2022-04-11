15c52a63287785503ddcdda098672dad12cc5167
peytonbrsmith <peytonbrsmith@gmail.com>
2022-04-11 12:17:31 -0500

    adjusted prototype

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Pascal's Triangle"""
 
-def pascal_triangle(i):
-    if (i <= 0):
+def pascal_triangle(n):
+    if (n <= 0):
         return []
     triangle = [[1]]
-    for n in range(1, i):
+    for n in range(1, n):
         row = [1]
         for k in range(1, n):
             row.append(triangle[n - 1][k - 1] + triangle[n - 1][k])
